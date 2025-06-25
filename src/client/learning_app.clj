81ab603d0886e7efc07f5c5e1bcf3c51f2a19578
Shundeev Egor <shundeevegor@gmail.com>
2025-06-25 19:01:34 +0200

    GH-38 Setup ServiceWorking Caching

---
+++ src/client/learning_app.clj
@@ -0,0 +1,10 @@
+(ns learning-app)
+
+
+(defmacro resources
+  [urls]
+  (let [resources-list (atom [])]
+    (doseq [url urls]
+      (let [revision (hash (slurp (str "resources/public" url)))]
+        (swap! resources-list conj {:url url :revision revision})))
+    @resources-list))
--- src/client/learning_app.cljs
+++ src/client/learning_app.cljs
@@ -1,4 +1,67 @@
-(ns learning-app)
+(ns learning-app
+  (:require-macros [learning-app :refer [resources]])
+  (:require
+   [lambdaisland.glogi :as log]
+   [lambdaisland.glogi.console :as glogi-console]
+   [promesa.core :as p]))
 
-(js/console.log "Hello world")
 
+(glogi-console/install!)
+
+(log/set-levels
+ {:glogi/root :all})
+
+
+(def resources
+  (resources
+   ["/index.html"
+    "/manifest.json"
+    "/favicon.ico"]))
+
+
+(js/self.addEventListener
+ "install"
+ (fn [event]
+   (..
+    event
+    (waitUntil
+     (p/let [cache (js/caches.open "resources")]
+       (p/doseq [{:keys [url revision]} resources]
+         (p/let [cached-response (.match cache url)]
+           (if (some? cached-response)
+             (p/let [body (.text cached-response)]
+               (when (not= (hash body) revision)
+                 (.add cache url)))
+             (p/promise
+              (.add cache url))))))))))
+
+
+(defn normalize-url
+  [url]
+  (.-href (js/URL. url js/self.location.origin)))
+
+
+(js/self.addEventListener
+ "activate"
+ (fn [event]
+   (..
+    event
+    (waitUntil
+     (p/do
+       (log/debug :activate "Cleaning up unused cache")
+       (p/let [cache           (js/caches.open "resources")
+               cached-requests (.keys cache)
+               resource-urls   (->> resources (map :url) (map normalize-url) set)]
+         (p/doseq [cached-request cached-requests]
+           (let [cached-url (.-url cached-request)]
+             (when-not (contains? resource-urls cached-url)
+               (.delete cache cached-url)))))
+
+       (log/debug :activate "Claiming the client")
+       (.claim js/self.clients))))))
+
+
+(js/self.addEventListener
+ "fetch"
+ (fn [event]
+   (log/debug :fetch ["Intercepting request event:" (js->clj event)])))
