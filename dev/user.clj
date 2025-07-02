0771f596836774b308c904ecd04fbed9ed813690
Shundeev Egor <shundeevegor@gmail.com>
2025-07-02 16:06:15 +0200

    GH-38 Fix Repl Startup
    slq-application-defined-classes were not created on repl starup

--- dev/user.clj
+++ dev/user.clj
@@ -1,3 +1,11 @@
 (ns user
   (:require
-   [debux.core]))
+   [debux.core]
+   [sqlite.application-defined-functions]))
+
+
+;; Improving Development Startup Time
+;; https://clojure.org/guides/dev_startup_time
+(binding [*compile-files* true       ;; compile during load
+          *compile-path*  "classes"]
+  (require 'user :reload-all))       ;; reload this and all transitively loaded namespaces
