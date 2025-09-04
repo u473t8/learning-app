c7500a99405b05528663a68ce390c4ab7338ee7a
Shundeev Egor <shundeevegor@gmail.com>
2025-09-04 17:19:02 +0200

    GH-38 Add parameters interceptor for clojurescript

---
+++ src/client/reitit/http/interceptors/parameters.cljs
@@ -0,0 +1,36 @@
+(ns reitit.http.interceptors.parameters 
+  (:require
+   [clojure.string :as str]
+   [lambdaisland.glogi :as log]
+   [promesa.core :as p]))
+
+
+(defn parameters-interceptor
+  "Interceptor to parse urlencoded parameters from the query string and form
+  body (if the request is a url-encoded form). Adds the following keys to
+  the request map:
+
+  :query-params - a map of parameters from the query string
+  :form-params  - a map of parameters from the body
+  :params       - a merged map of all types of parameter"
+  []
+  {:name ::parameters
+   :enter (fn [ctx]
+            (let [query-params (some-> ctx
+                                       :request
+                                       :query-string
+                                       js/URLSearchParams.
+                                       .entries
+                                       js/Object.fromEntries
+                                       (js->clj :keywordize-keys true))
+                  content-type (-> ctx :request :headers (get "content-type"))]
+              (log/debug :parameters-interceptor/enter {"Content-Type" content-type})
+              (if (some-> content-type (str/includes? "application/x-www-form-urlencoded"))
+                (p/let [form-data (-> ctx :request :js/request .formData) ; a promise
+                        form-data (-> form-data .entries js/Object.fromEntries (js->clj :keywordize-keys true))]
+                  (log/debug :parameters-interceptor/enter {"Form-Data" form-data})
+                  ;; original ring.middleware.params middleware merges form-data and query-params in the same order.
+                  (update ctx :request merge {:query-params query-params :form-params form-data, :params (merge form-data query-params)}))
+                ctx)))})
+
+
