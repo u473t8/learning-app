97b294b6822540b068d2bb4d77dbfc2573dc39cc
Shundeev Egor <shundeevegor@gmail.com>
2026-02-13 17:41:30 +0100

    GH-90 [ui] Restructure home layout and pin lesson CTA footer

--- src/client/views/home.cljs
+++ src/client/views/home.cljs
@@ -3,42 +3,45 @@
 
 
 (defn page
-  [& _]
+  []
   [:div.home
-   [:header.home__hero
-    [:div.home__hero-text
-     [:p.home__subtitle
-      "Быстро добавляйте слова и учите немецкий даже без сети."]]]
+   [:header.home__intro
+    [:h1.home__title
+     "Главная"]
+    [:p.home__subtitle
+     "Быстро добавляйте слова и учите немецкий даже без сети."]]
 
-   [:section.home__grid
-    [:div.home__add-stack
-     [:div.home__footer-actions
-      [:button.home__lesson-button.big-button.green-button
-       {:hx-get       "/lesson"
-        :hx-indicator "#loader"
-        :hx-push-url  "true"
-        :hx-swap      "innerHTML"
-        :hx-target    "#app"}
-       "НАЧАТЬ УРОК"]]
-     [:section.home__panel.home__panel--add
-      [:div.home__add-header
-       [:h2.home__panel-title
-        "Быстрое добавление"]
-       [:button.home__words-button
-        {:hx-get "/words" :hx-push-url "true" :hx-swap "innerHTML" :hx-target "#app"}
-        "Список слов"]]
-      [:form.new-word-form.new-word-form--quick
-       {:hx-on:htmx:after-request
-        "if(event.detail.successful && event.detail.elt===this) {this.reset();}"
-        :hx-post         "/words"
-        :hx-push-url     "false"
-        :hx-swap         "none"
-        :hx-disabled-elt "find .new-word-form__submit"
-        :hx-disinherit   "hx-disabled-elt"}
+   [:main.home__content
+    [:section.home__add
+     [:header.home__add-header
+      [:h2.home__panel-title
+       "Быстрое добавление"]
+      [:button.home__words-button
+       {:type        "button"
+        :hx-get      "/words"
+        :hx-push-url "true"
+        :hx-swap     "innerHTML"
+        :hx-target   "#app"}
+       "Список слов"]]
+
+     [:form.home__add-form
+      {:hx-on:htmx:after-request
+       "if(event.detail.successful && event.detail.elt===this) {this.reset();}"
+       :hx-post         "/words"
+       :hx-push-url     "false"
+       :hx-swap         "none"
+       :hx-disabled-elt "find button[type='submit']"
+       :hx-disinherit   "hx-disabled-elt"}
+      [:fieldset.home__add-fieldset
+       [:legend.home__add-legend
+        "Добавить слово"]
        [:word-autocomplete
-        [:div.new-word-form__row
+        [:div.home__add-form-row
          [:div.autocomplete
-          [:input.new-word-form__input.new-word-form__input--quick
+          [:label.home__add-form-label
+           {:for "new-word-value"}
+           "Слово (немецкий)"]
+          [:input.home__add-form-input
            {:id             "new-word-value"
             :name           "value"
             :autocapitalize "off"
@@ -57,18 +60,34 @@
             :placeholder    "Новое слово"}]
           [:ul.suggestions
            {:data-ac-role "list"}]]
-         [:span.new-word-form__arrow
+         [:span.home__add-form-arrow
           "→"]
-         [:input.new-word-form__input.new-word-form__input--quick
-          {:id           "new-word-translation"
-           :name         "translation"
-           :autocapitalize "off"
-           :autocomplete "off"
-           :autocorrect  "off"
-           :data-ac-role "translation"
-           :lang         "ru"
-           :placeholder  "Перевод"
-           :required     true}]]]
-       [:button.new-word-form__submit.big-button
+         [:div.home__add-translation
+          [:label.home__add-form-label
+           {:for "new-word-translation"}
+           "Перевод (русский)"]
+          [:input.home__add-form-input
+           {:id           "new-word-translation"
+            :name         "translation"
+            :autocapitalize "off"
+            :autocomplete "off"
+            :autocorrect  "off"
+            :data-ac-role "translation"
+            :lang         "ru"
+            :placeholder  "Перевод"
+            :required     true}]]]]
+
+       [:button.home__add-form-submit.big-button.big-button--request-stable
         {:type "submit"}
-        "ДОБАВИТЬ"]]]]]])
+        "ДОБАВИТЬ"]]]]]
+
+   [:footer.home__footer
+    [:h2.home__lesson-title
+     "Урок"]
+    [:button.home__lesson-button.big-button.green-button
+     {:hx-get       "/lesson"
+      :hx-indicator "#loader"
+      :hx-push-url  "true"
+      :hx-swap      "innerHTML"
+      :hx-target    "#app"}
+     "НАЧАТЬ УРОК"]]])
